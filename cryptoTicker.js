6aa2501937429501a5bd36855a5048afaeb28fa9
Paul Fitzgerald <paulfitz99@gmail.com>
2019-09-26 09:50:03 +0100

    initial commit

---
+++ cryptoTicker.js
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styled, { keyframes } from "styled-components";
+
+const customTicker = keyframes`
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(-1000%, 0, 0);
+  }
+`;
+
+const CryptosWrapper = styled.div`
+  background: #123962;
+  overflow: hidden;
+`;
+
+const TickerWrap = styled.div`
+  display: flex;
+  overflow: hidden;
+`;
+
+const CryptoWrapper = styled.div`
+  color: white;
+  flex: 1 0 100px;
+  animation: ${customTicker} 7s infinite;
+  animation-timing-function: linear;
+`;
+
+const Currency = styled.span`
+  color: #5d81a6;
+`;
+
+const Heading = styled.p`
+  font-size: 12px;
+`;
+
+function App() {
+  const [cryptos, setCryptos] = React.useState(null);
+  React.useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        setCryptos([...res.data.slice(0, 10), ...res.data.slice(0, 10)]);
+      });
+  }, []);
+  return (
+    <CryptosWrapper>
+      <TickerWrap>
+        {cryptos &&
+          cryptos.map((crypto, index) => {
+            return (
+              <CryptoWrapper key={index}>
+                <Heading>{crypto.symbol}/USD</Heading>
+                <p>
+                  {parseFloat(crypto.priceUsd)
+                    .toFixed(2)
+                    .toLocaleString("en-US")}{" "}
+                  <Currency>USD</Currency>
+                </p>
+              </CryptoWrapper>
+            );
+          })}
+      </TickerWrap>
+    </CryptosWrapper>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
